8645f28ce61cf8b5ff4a577434435b70720a3f35
Nicole Url <nicole.url.4@gmail.com>
2016-12-13 15:47:50 -0800

    Added diagonal win conditional and test

--- spec/tic_tac_toe.spec.js
+++ spec/tic_tac_toe.spec.js
@@ -3,14 +3,22 @@ import Game from 'tic_tac_toe';
 
 describe('Game', function() {
     var blankGame = new Game();
+
     var horizontalWinningGame = new Game();
-    horizontalWinningGame.board = [
-      [ null, 1, 1 ],
-      [ null, 0, 0 ],
-      [ 1, 1, 1 ]
-    ];
-    // console.log("########");
-    // console.log(blankGame.board);
+
+        horizontalWinningGame.board = [
+          [ null, "x", "x" ],
+          [ null, "o", "o" ],
+          [ "x", "x", "x" ]
+        ];
+
+    var diagonalWinningGame = new Game();
+        diagonalWinningGame.board = [
+          [ "x", "o", null ],
+          [ "o", "x", null ],
+          [ "o", "o", "x" ]
+        ];
+
 
     it('should be an array of three arrays', function() {
       expect(blankGame.board.length).toEqual(3);
@@ -19,8 +27,13 @@ describe('Game', function() {
     it('should be an array', function () {
       expect(blankGame.board).toEqual(jasmine.any(Array));
     });
+
     it('should know if there is a horizontal win', function() {
-      expect(horizontalWinningGame.checkIfWon()).toBeTruthy();
+      expect(horizontalWinningGame.checkIfWon()).toEqual(true);
+    });
+
+    it('should know if there is a diagonal win', function() {
+      expect(diagonalWinningGame.checkIfWon()).toEqual(true);
     });
 
 });
--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -10,13 +10,20 @@ var Game = function () {
 };
 
 Game.prototype.checkIfWon = function() {
-  // console.log("YOLO!!!!!");
-  // console.log(this.board);
+
   for(var i = 0; i < this.board.length; i++) {
-    if((this.board[i][0] == this.board[i][1] == this.board[i][2]) && (this.board[i][0] !== null)) {
-      return true;
+    // Check horizontal win
+    if (
+      (this.board[i][0] == this.board[i][1]) &&
+      (this.board[i][0] == this.board[i][2]) &&
+      (this.board[i][0] !== null) ) {
+        return true;
+    } else if ( ((this.board[0][0] == this.board[1][1]) && (this.board[0][0] == this.board[2][2])) ||
+      ((this.board[0][2] == this.board[1][1]) && (this.board[0][2] == this.board[2][0])) ) {
+        return true;
     }
   }
+  return false;
 };
 
 Game.prototype.determinePlayer = function(xPlay) {
