9e3b913ec769d732bec734d6b75a3b718d47c85b
morrime <marymorrison15@gmail.com>
2016-12-13 15:48:14 -0800

    vertical win logic working, wrote tests for null values in vertical and horizontal win cases

--- spec/tic_tac_toe.spec.js
+++ spec/tic_tac_toe.spec.js
@@ -3,11 +3,33 @@ import Game from 'tic_tac_toe';
 
 describe('Game', function() {
     var blankGame = new Game();
+
     var horizontalWinningGame = new Game();
     horizontalWinningGame.board = [
-      [ null, 1, 1 ],
-      [ null, 0, 0 ],
-      [ 1, 1, 1 ]
+      [ null, "x", "x" ],
+      [ null, "o", "o" ],
+      [ "x", "x", "x" ]
+    ];
+
+    var horizontalNullGame = new Game();
+    horizontalNullGame.board = [
+      [ null, null, null ],
+      [ null, "o", "o" ],
+      [ "x", "x", "o" ]
+    ];
+
+    var verticalWinningGame = new Game();
+    verticalWinningGame.board = [
+      [ "o", null, "x" ],
+      [ null, "o", "x" ],
+      [ "x", "o", "x" ]
+    ];
+
+    var verticalNullGame = new Game();
+    verticalNullGame.board = [
+      [ "o", null, null ],
+      [ null, "x", null ],
+      [ "x", "o", null ]
     ];
     // console.log("########");
     // console.log(blankGame.board);
@@ -19,8 +41,20 @@ describe('Game', function() {
     it('should be an array', function () {
       expect(blankGame.board).toEqual(jasmine.any(Array));
     });
+
     it('should know if there is a horizontal win', function() {
-      expect(horizontalWinningGame.checkIfWon()).toBeTruthy();
+      expect(horizontalWinningGame.checkIfWon()).toEqual(true);
     });
 
+    it('should know to return false if null is in horizontal place', function() {
+      expect(horizontalNullGame.checkIfWon()).toEqual(false);
+    });
+
+    it('should know if there is a vertical win', function() {
+      expect(verticalWinningGame.checkIfWon()).toEqual(true);
+    });
+
+    it('should know to return false if null is in vertical place', function() {
+      expect(verticalNullGame.checkIfWon()).toEqual(false);
+    });
 });
--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -13,10 +13,13 @@ Game.prototype.checkIfWon = function() {
   // console.log("YOLO!!!!!");
   // console.log(this.board);
   for(var i = 0; i < this.board.length; i++) {
-    if((this.board[i][0] == this.board[i][1] == this.board[i][2]) && (this.board[i][0] !== null)) {
+    if((this.board[i][0] == this.board[i][1]) && (this.board[i][0] == this.board[i][2]) && (this.board[i][0] !== null)) {
+      return true;
+    } else if ((this.board[0][i] == this.board[1][i]) && (this.board[0][i] == this.board[2][i]) && (this.board[0][i] !== null)) {
       return true;
     }
   }
+  return false;
 };
 
 Game.prototype.determinePlayer = function(xPlay) {
