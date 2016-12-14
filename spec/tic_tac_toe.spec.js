e787f17d7c8273c20536c2b3e7c7329717e5941c
morrime <marymorrison15@gmail.com>
2016-12-14 10:48:36 -0800

    fixed tests after changing determinePlayer and checkIfWon to be attributes of Game, instead of prototypes/independent functions

--- spec/tic_tac_toe.spec.js
+++ spec/tic_tac_toe.spec.js
@@ -4,35 +4,21 @@ import Game from 'tic_tac_toe';
 describe('Game', function() {
   var blankGame = new Game();
 
-  var horizontalWinningGame = new Game(),
+  var horizontalWinningGame = new Game();
       horizontalWinningGame.board = [
         [ null, "x", "x" ],
         [ null, "o", "o" ],
         [ "x", "x", "x" ]
       ];
 
-  var horizontalNullGame = new Game(),
-      horizontalNullGame.board = [
-        [ null, null, null ],
-        [ null, "o", "o" ],
-        [ "x", "x", "o" ]
-      ];
-
-  var verticalWinningGame = new Game(),
+  var verticalWinningGame = new Game();
       verticalWinningGame.board = [
         [ "o", null, "x" ],
         [ null, "o", "x" ],
         [ "x", "o", "x" ]
       ];
 
-  var verticalNullGame = new Game(),
-      verticalNullGame.board = [
-        [ "o", null, null ],
-        [ null, "x", null ],
-        [ "x", "o", null ]
-      ];
-
-  var diagonalWinningGame = new Game(),
+  var diagonalWinningGame = new Game();
       diagonalWinningGame.board = [
         [ "x", "o", null ],
         [ "o", "x", null ],
@@ -52,7 +38,7 @@ describe('Game', function() {
   });
 
   it('should know to return false if null is in horizontal place', function() {
-    expect(horizontalNullGame.checkIfWon()).toEqual(false);
+    expect(blankGame.checkIfWon()).toEqual(false);
   });
 
   it('should know if there is a vertical win', function() {
@@ -60,11 +46,14 @@ describe('Game', function() {
   });
 
   it('should know to return false if null is in vertical place', function() {
-    expect(verticalNullGame.checkIfWon()).toEqual(false);
+    expect(blankGame.checkIfWon()).toEqual(false);
   });
 
   it('should know if there is a diagonal win', function() {
     expect(diagonalWinningGame.checkIfWon()).toEqual(true);
   });
 
+  it('should know to return false if null is in middle place(testing diagonal win case with nulls)', function() {
+    expect(blankGame.checkIfWon()).toEqual(false);
+  });
 });
--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -8,54 +8,54 @@ var Game = function () {
   // determine whose turn it is
   this.xPlay = true;
   console.log("Welcome to TIC TAC TOE!");
-};
-
-checkIfWon = function(board) {
-  for(var i = 0; i < board.length; i++) {
-    // horizontal win
-    if (
-      (board[i][0] == board[i][1]) &&
-      (board[i][0] == board[i][2]) &&
-      (board[i][0] !== null) ) {
-        return true;
-    // vertical win
-    } else if (
-      (board[0][i] == board[1][i]) && (board[0][i] == board[2][i]) && (board[0][i] !== null)) {
-      return true;
-    // diagonal win
-    } else if (
-      ( (board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && (board[1][1] !== null) )
-      ||
-      ( (board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) && (board[1][1] !== null) )
-    ) {
-        return true;
+  this.determinePlayer = function() {
+    // print current game board
+    console.log("determinePlayer board (below)");
+    console.log(this.board);
+    // print whose turn it is
+    if ( this.xPlay === true ) {
+      console.log("Player X, it's your turn");
+    } else {
+      console.log("Player O, it's your turn");
     }
-  }
-  // no win
-  return false;
+  };
+  this.checkIfWon = function() {
+    for(var i = 0; i < this.board.length; i++) {
+      // horizontal win
+      if (
+        (this.board[i][0] == this.board[i][1]) &&
+        (this.board[i][0] == this.board[i][2]) &&
+        (this.board[i][0] !== null) ) {
+          return true;
+      // vertical win
+      } else if (
+        (this.board[0][i] == this.board[1][i]) && (this.board[0][i] == this.board[2][i]) && (this.board[0][i] !== null)) {
+        return true;
+      // diagonal win
+      } else if (
+        ( (this.board[0][0] == this.board[1][1]) && (this.board[0][0] == this.board[2][2]) && (this.board[1][1] !== null) )
+        ||
+        ( (this.board[0][2] == this.board[1][1]) && (this.board[0][2] == this.board[2][0]) && (this.board[1][1] !== null) )
+      ) {
+          return true;
+      }
+    }
+    // no win
+    return false;
+  };
 };
 
-determinePlayer = function(xPlay, board) {
-  // print current game board
-  console.log("determinePlayer board (below)");
-  console.log(board);
-  // print whose turn it is
-  if ( xPlay === true ) {
-    console.log("Player X, it's your turn");
-  } else {
-    console.log("Player O, it's your turn");
-  }
-};
+
 
 Game.prototype.consolePlay = function(row, column) {
   // print player and board
-  determinePlayer(this.xPlay, this.board);
+  this.determinePlayer();
 
   // if spot isn't occupied, mark spot with correct letter for player "x"
   if((this.board[row-1][column-1] === null) && (this.xPlay === true)) {
     this.board[row-1][column-1] = "x";
     // print winner if that marking results in a win
-    if ( checkIfWon(this.board) === true) {
+    if ( this.checkIfWon() === true) {
       console.log("X WINS!");
       console.log(checkIfWon());
     // if no resulting win, switch turns
@@ -66,7 +66,7 @@ Game.prototype.consolePlay = function(row, column) {
   } else if ((this.board[row-1][column-1] === null) && (this.xPlay === false)) {
     this.board[row-1][column-1] = "o";
     // print winner if that marking results in a win
-    if (checkIfWon(this.board) === true) {
+    if (this.checkIfWon() === true) {
       console.log("O WINS");
     // if no resulting win, switch turns
     } else {
@@ -76,7 +76,7 @@ Game.prototype.consolePlay = function(row, column) {
   } else {
     console.log("NO.");
     console.log("invalid play board");
-    determinePlayer(this.xPlay, this.board);
+    this.determinePlayer();
   }
 };
 
@@ -84,4 +84,4 @@ Game.prototype.consolePlay = function(row, column) {
 // game.consolePlay(1,3) //player x
 // console.log(game.board);
 
-// export default Game;
+export default Game;
