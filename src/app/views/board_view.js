f0bbcd11f7696095e13b8bcf7c1232b6d5ff6e9f
morrime <marymorrison15@gmail.com>
2016-12-19 12:13:01 -0800

    WE ARE THE CHAMPIONS. Wave 2 complete. got click handler to log the id for the clicked element, added some basic css

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -6,14 +6,19 @@ const BoardView = Backbone.View.extend({
   initialize: function(options) {
     this.template = options.template;
     this.board = options.model;
-    console.log("#############");
-    console.log(this.board.attributes);
   },
   render: function(){
     var html = this.template({board: this.board.attributes.layout});
     this.$el.html(html);
 
     return this;
+  },
+  events: {
+    "click": "clickText"
+  },
+
+  clickText: function(event) {
+    console.log(event.target.id);
   }
 
 });
